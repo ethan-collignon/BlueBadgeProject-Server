52340be485a809ed76787bb90719fe9106a254b4
JamesTodhunter <90239716+JamesTodhunter@users.noreply.github.com>
2021-10-30 14:13:10 -0600

    first commit

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const Express = require('express');
 const app = Express();
 const dbConnection = require("./db");
-
-app.use(Express.json());
-
+const middleware = require("./middleware");
+app.use(require('./middleware/headers'));
 const controllers = require("./controllers");
-
+app.use(Express.json());
 app.use("/user", controllers.userController);
-app.use(require("./middleware/validate-jwt"))
 app.use("/review", controllers.reviewController);
-
 dbConnection.authenticate()
   .then(() => dbConnection.sync())
   .then(() => {
@@ -21,3 +18,4 @@ dbConnection.authenticate()
   .catch((err) => {
     console.log(`[Server]: Server crashed. Error = ${err}`);
   });
+app.use(middleware.CORS);
